8fa58d2aa52ec6c7201263ea42c78793c50422bb
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2020-09-08 21:48:24 +0900

    user register

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,18 +4,25 @@
 
   <ul class="navbar-nav ml-auto">
 
+    @guest
     <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a> 
     </li>
+    @endguest
 
+    @guest
     <li class="nav-item">
       <a class="nav-link" href=""><i class="fas fa-door-open"></i>ログイン</a>
     </li>
-
+    @endguest
+      
+    @auth
     <li class="nav-item">
       <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
-
+    @endauth
+    
+    @auth
     <!-- Dropdown -->
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
@@ -33,8 +40,10 @@
         </button>
       </div>
     </li>
-    <form id="logout-button" method="POST" action="">
+    <form id="logout-button" method="POST" action="{{ route('logout') }}"> {{--この行を編集--}}
+      @csrf
     </form>
+    @endauth
     <!-- Dropdown -->
 
   </ul>
