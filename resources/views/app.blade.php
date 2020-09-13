6381f78bf5aef2bc5ea5366f25e2ce0b2c6245ba
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2020-09-13 15:07:09 +0900

    background-repeat

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,7 +42,7 @@
 
       /* Center and scale the image nicely */
       background-position: center;
-      background-repeat: no-repeat;
+      background-repeat: repeat-y;
       background-size: cover;
       
     }
